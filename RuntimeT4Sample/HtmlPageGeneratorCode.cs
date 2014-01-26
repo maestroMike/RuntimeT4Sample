d15f5da76746740db67bd222c82c0b6f1b0ef74f
Mike Bardynin <mikebardynin@gmail.com>
2014-01-26 21:23:56 +0400

    add: сам пример

---
+++ RuntimeT4Sample/HtmlPageGeneratorCode.cs
@@ -0,0 +1,12 @@
+﻿// Mike Bardynin [mikebardynin@gmail.com]
+namespace RuntimeT4Sample
+{
+public partial class HtmlPageGenerator
+{
+	private string _customText;
+	public HtmlPageGenerator(string customText)
+	{
+			_customText = customText;
+	}
+}
+}
---
+++ RuntimeT4Sample/Program.cs
@@ -0,0 +1,23 @@
+﻿// Mike Bardynin [mikebardynin@gmail.com]
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace RuntimeT4Sample
+{
+public static class Program
+{
+	public static void Main(params string[] args)
+	{
+		Console.WriteLine( "Enter any words end press enter to generate html page." );
+		var userText = Console.ReadLine();
+
+		var generatedHtml = new HtmlPageGenerator( userText ).TransformText();
+
+		var filePath = Path.Combine( Path.GetTempPath(), "htmlReport.html" );
+		File.WriteAllText( filePath, generatedHtml );
+		Process.Start( filePath );
+	}
+}
+}
